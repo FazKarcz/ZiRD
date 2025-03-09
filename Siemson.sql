1fbac9487ec6feb56cd15b76d9ae71498d9489bf
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2025-03-09 14:49:26 +0100

    Delete Siemson.sql

--- Siemson.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE JOBS (
-    job_id INT NOT NULL,
-    job_title VARCHAR2(35),
-    min_salary FLOAT CHECK (min_salary < max_salary AND min_salary >= 2000),
-    max_salary FLOAT
-    );
